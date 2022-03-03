fa2d7c3fc821904cc5cc71addb5cc6c9bf824f36
prokid20000 <95936374+prokid20000@users.noreply.github.com>
2022-03-03 15:30:57 -0500

    In newboxform

--- src/NewBoxForm.js
+++ src/NewBoxForm.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { v4 as uuid } from 'uuid';
 
-function NewBoxForm() {
+function NewBoxForm({ addBox }) {
+    const [formData, setFormData] = useState({
+        height: "",
+        width: "",
+        backgroundColor: "",
+    });
+
+    /**Update form input */
+    function handleChange(evt) {
+        const { name, value } =evt.target;
+        setFormData(formData => ({...formData, [name]: value}));
+    }
+
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        addBox(formData);
+        setFormData({
+            height: "",
+            width: "",
+            backgroundColor: "",
+        });
+    }
+
+    return (<div>
+        <form>
+            <label for="height-input">Height:</label>
+            <input id="height-input" name="height" />
+            <label for="width-input">Width:</label>
+            <input id="width-input" name="width" />
+            <label for="background-color-input">Background Color:</label>
+            <input id="background-color-input" name="backgroundColor" />
+
+            <button>Add!</button>
+        </form>
+    </div>)
 
 }
 
